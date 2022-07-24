6d9dd3e978ae9f8f50f1780a2ced5d1c71190a5a
三刀 <zhengjunweimail@163.com>
2022-07-24 20:08:38 +0800

    移除ClientAuthorizeProvider

--- smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/plugin/provider/Providers.java
+++ smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/plugin/provider/Providers.java
@@ -4,27 +4,16 @@ import org.smartboot.mqtt.broker.persistence.message.MemoryPersistenceProvider;
 import org.smartboot.mqtt.broker.persistence.message.PersistenceProvider;
 import org.smartboot.mqtt.broker.persistence.session.MemorySessionStateProvider;
 import org.smartboot.mqtt.broker.persistence.session.SessionStateProvider;
-import org.smartboot.mqtt.broker.plugin.provider.mock.MockClientAuthorizeProvider;
 
 /**
  * @author 三刀（zhengjunweimail@163.com）
  * @version V1.0 , 2022/4/1
  */
 public class Providers {
-    private ClientAuthorizeProvider clientAuthorizeProvider = new MockClientAuthorizeProvider();
     private SessionStateProvider sessionStateProvider = new MemorySessionStateProvider();
 
     private PersistenceProvider retainMessageProvider = new MemoryPersistenceProvider();
     private PersistenceProvider persistenceProvider = new MemoryPersistenceProvider();
-
-    public ClientAuthorizeProvider getClientAuthorizeProvider() {
-        return clientAuthorizeProvider;
-    }
-
-    public void setClientAuthorizeProvider(ClientAuthorizeProvider clientAuthorizeProvider) {
-        this.clientAuthorizeProvider = clientAuthorizeProvider;
-    }
-
 
     public SessionStateProvider getSessionStateProvider() {
         return sessionStateProvider;
