a835c197ca89955964a14de50fb13acce5fa53dd
三刀 <zhengjunweimail@163.com>
2022-09-10 11:33:22 +0800

    正式版：v0.7

--- smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/BrokerConfigure.java
+++ smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/BrokerConfigure.java
@@ -27,7 +27,7 @@ public class BrokerConfigure {
     /**
      * 当前smart-mqtt
      */
-    public static final String VERSION = "v0.7-SNAPSHOT";
+    public static final String VERSION = "v0.7";
 
     static final Map<String, String> SystemEnvironments = new HashMap<>();
 
