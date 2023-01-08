a01c594510f5f57fd274111508bdeca718eacea8
三刀 <zhengjunweimail@163.com>
2023-01-08 14:22:16 +0800

    开发mqtt5.0

--- smart-mqtt-common/src/main/java/org/smartboot/mqtt/common/message/MqttPublishMessage.java
+++ smart-mqtt-common/src/main/java/org/smartboot/mqtt/common/message/MqttPublishMessage.java
@@ -57,79 +57,6 @@ public class MqttPublishMessage extends MqttVariableMessage<MqttPublishVariableH
         setVariableHeader(variableHeader);
     }
 
-//    private void decodePublishProperties(ByteBuffer buffer, PublishProperties publishProperties) {
-//        int remainingLength = MqttCodecUtil.decodeVariableByteInteger(buffer);
-//        if (remainingLength == 0) {
-//            return;
-//        }
-//
-//        int topicAlias = -1;
-//        int subscriptionIdentifier = -1;
-//        int messageExpiryInterval = -1;
-//        byte payloadFormatIndicator = -1;
-//        int position;
-//        while (remainingLength > 0) {
-//            position = buffer.position();
-//            switch (buffer.get()) {
-//                //载荷格式指示
-//                case MqttPropertyConstant.PAYLOAD_FORMAT_INDICATOR:
-//                    //包含多个载荷格式指示（Payload Format Indicator）将造成协议错误（Protocol Error）
-//                    ValidateUtils.isTrue(payloadFormatIndicator == -1, "");
-//                    payloadFormatIndicator = buffer.get();
-//                    publishProperties.setPayloadFormatIndicator(payloadFormatIndicator);
-//                    ValidateUtils.isTrue(payloadFormatIndicator == 0 || payloadFormatIndicator == 1, "");
-//                    break;
-//                //消息过期间隔
-//                case MqttPropertyConstant.MESSAGE_EXPIRY_INTERVAL:
-//                    //包含多个消息过期间隔将导致协议错误（Protocol Error）
-//                    ValidateUtils.isTrue(messageExpiryInterval == -1, "");
-//                    messageExpiryInterval = buffer.getInt();
-//                    publishProperties.setMessageExpiryInterval(messageExpiryInterval);
-//                    ValidateUtils.isTrue(messageExpiryInterval > 0, "");
-//                    break;
-//                //最大报文长度
-//                case MqttPropertyConstant.TOPIC_ALIAS:
-//                    //包含多个主题别名值将造成协议错误（Protocol Error）。
-//                    ValidateUtils.isTrue(topicAlias == -1, "");
-//                    topicAlias = decodeMsbLsb(buffer);
-//                    publishProperties.setTopicAlias(decodeMsbLsb(buffer));
-//                    //todo
-//                    break;
-//                //响应主题
-//                case MqttPropertyConstant.RESPONSE_TOPIC:
-//                    ValidateUtils.isTrue(publishProperties.getResponseTopic() == null, "");
-//                    publishProperties.setResponseTopic(decodeString(buffer));
-//                    break;
-//                //请求响应信息
-//                case MqttPropertyConstant.CORRELATION_DATA:
-//                    ValidateUtils.isTrue(publishProperties.getCorrelationData() == null, "");
-//                    publishProperties.setCorrelationData(decodeByteArray(buffer));
-//                    break;
-//                //用户属性
-//                case MqttPropertyConstant.USER_PROPERTY:
-//                    String key = decodeString(buffer);
-//                    String value = decodeString(buffer);
-//                    publishProperties.getUserProperties().add(new UserProperty(key, value));
-//                    break;
-//                //订阅标识符
-//                case MqttPropertyConstant.SUBSCRIPTION_IDENTIFIER:
-//                    //包含多个订阅标识符将造成协议错误（Protocol Error）
-//                    ValidateUtils.isTrue(subscriptionIdentifier == -1, "");
-//                    subscriptionIdentifier = buffer.getInt();
-//                    publishProperties.setSubscriptionIdentifier(subscriptionIdentifier);
-//                    //订阅标识符取值范围从1到268,435,455
-//                    ValidateUtils.isTrue(subscriptionIdentifier >= 1 && subscriptionIdentifier <= 268435455, "");
-//                    break;
-//                //内容类型
-//                case MqttPropertyConstant.CONTENT_TYPE:
-//                    ValidateUtils.isTrue(publishProperties.getContentType() == null, "");
-//                    publishProperties.setContentType(decodeString(buffer));
-//                    break;
-//            }
-//            remainingLength -= buffer.position() - position;
-//        }
-//    }
-
     @Override
     public void decodePlayLoad(ByteBuffer buffer) {
         int remainingLength = fixedHeader.remainingLength();
@@ -153,7 +80,7 @@ public class MqttPublishMessage extends MqttVariableMessage<MqttPublishVariableH
         int propertiesLength = 0;
         if (version == MqttVersion.MQTT_5) {
             //属性长度
-            propertiesLength = propertiesLength(variableHeader.getPublishProperties());
+            propertiesLength = preEncodeProperties(variableHeader.getPublishProperties());
             length += getVariableLengthInt(propertiesLength) + propertiesLength;
         }
         mqttWriter.write(encodeMBI(length));
@@ -170,7 +97,7 @@ public class MqttPublishMessage extends MqttVariableMessage<MqttPublishVariableH
         mqttWriter.write(payload);
     }
 
-    private int propertiesLength(PublishProperties properties) {
+    private int preEncodeProperties(PublishProperties properties) {
         if (properties == null) {
             return 0;
         }
