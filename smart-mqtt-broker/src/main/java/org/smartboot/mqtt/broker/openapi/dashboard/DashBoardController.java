998e11b8a921da3de92b5a27411a3b0c74b0cdbf
三刀 <zhengjunweimail@163.com>
2023-01-22 21:56:22 +0800

    openapi开发

--- smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/openapi/dashboard/DashBoardController.java
+++ smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/openapi/dashboard/DashBoardController.java
@@ -1,5 +1,6 @@
 package org.smartboot.mqtt.broker.openapi.dashboard;
 
+import com.sun.management.OperatingSystemMXBean;
 import jdk.jfr.internal.JVM;
 import org.smartboot.http.restful.RestResult;
 import org.smartboot.http.restful.annotation.Controller;
@@ -8,6 +9,7 @@ import org.smartboot.http.server.HttpResponse;
 import org.smartboot.mqtt.broker.BrokerConfigure;
 import org.smartboot.mqtt.broker.openapi.OpenApi;
 
+import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.List;
 
@@ -40,32 +42,16 @@ public class DashBoardController {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
 
-        BrokerNodeTO brokerNodeTO = new BrokerNodeTO();
-        brokerNodeTO.setNode("smart-mqtt@192.168.1.1");
-        brokerNodeTO.setStatus(1);
-        brokerNodeTO.setVersion(BrokerConfigure.VERSION);
-        brokerNodeTO.setPid(JVM.getJVM().getPid());
-        brokerNodeTO.setCpu(50 + (int) (Math.random() * 50));
-        brokerNodeTO.setMemory(50 + (int) (Math.random() * 50));
+        BrokerNodeTO nodeTO = new BrokerNodeTO();
+        nodeTO.setNode("smart-mqtt@192.168.1.3");
+        nodeTO.setStatus(1);
+        nodeTO.setVersion(BrokerConfigure.VERSION);
+        nodeTO.setPid(JVM.getJVM().getPid());
 
-        BrokerNodeTO brokerNodeTO2 = new BrokerNodeTO();
-        brokerNodeTO2.setNode("smart-mqtt@192.168.1.2");
-        brokerNodeTO2.setStatus(2);
-        brokerNodeTO2.setVersion(BrokerConfigure.VERSION);
-        brokerNodeTO2.setPid(JVM.getJVM().getPid());
-        brokerNodeTO2.setCpu(50 + (int) (Math.random() * 50));
-        brokerNodeTO2.setMemory(50 + (int) (Math.random() * 50));
-
-        BrokerNodeTO brokerNodeTO3 = new BrokerNodeTO();
-        brokerNodeTO3.setNode("smart-mqtt@192.168.1.3");
-        brokerNodeTO3.setStatus(1);
-        brokerNodeTO3.setVersion(BrokerConfigure.VERSION);
-        brokerNodeTO3.setPid(JVM.getJVM().getPid());
-        brokerNodeTO3.setCpu(50 + (int) (Math.random() * 50));
-        brokerNodeTO3.setMemory(50 + (int) (Math.random() * 50));
-
-//        OperatingSystemMXBean osMxBean = ManagementFactory.getOperatingSystemMXBean();
-        return RestResult.ok(Arrays.asList(brokerNodeTO, brokerNodeTO2, brokerNodeTO3));
+        OperatingSystemMXBean systemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        nodeTO.setCpu((int) (systemMXBean.getSystemCpuLoad() * 100));
+        nodeTO.setMemory((int) ((systemMXBean.getTotalPhysicalMemorySize()+systemMXBean.getTotalSwapSpaceSize()-systemMXBean.getFreeSwapSpaceSize() - systemMXBean.getFreePhysicalMemorySize()) * 100.0 / (systemMXBean.getTotalPhysicalMemorySize()+systemMXBean.getTotalSwapSpaceSize())));
+        return RestResult.ok(Arrays.asList(nodeTO));
     }
 
 }
