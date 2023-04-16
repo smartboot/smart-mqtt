2b3b24f0e935c658d7ef4a4f0fcc02be49fb749c
三刀 <zhengjunweimail@163.com>
2023-04-16 15:01:59 +0800

    优化页面

--- dashboard/src/views/Dashboard/nodes.vue
+++ dashboard/src/views/Dashboard/nodes.vue
@@ -36,29 +36,31 @@
         </lay-table>
     </lay-card>
     <lay-layer type="drawer" v-model="modalVisible" title="MQTT Broker配置">
-        <lay-card>
-            <lay-button type="primary">保存并重启</lay-button>
-            <lay-button type="default" @click="">仅保存</lay-button>
-        </lay-card>
+        <lay-result status="failure" describe="开发中..."
+                    title="提示"></lay-result>
+<!--        <lay-card>-->
+<!--            <lay-button type="primary">保存并重启</lay-button>-->
+<!--            <lay-button type="default" @click="">仅保存</lay-button>-->
+<!--        </lay-card>-->
 
-        <lay-tab type="card" v-model="currentTab">
-            <lay-tab-item v-for="(n, index) in brokerConfig" :title="n.title" :id="index">
-                <lay-form :model="brokerForm">
-                    <lay-card v-if="n.dynamic_config&&n.dynamic_config.length>0" title="动态生效">
-                        <lay-form-item v-for="(item, i) in n.dynamic_config" :label="item.label" :prop="item.prop"
-                                       mode="inline">
-                            <lay-input :placeholder="item.placeholder"/>
-                        </lay-form-item>
-                    </lay-card>
-                    <lay-card v-if="n.restart_config&&n.restart_config.length>0" title="重启生效">
-                        <lay-form-item v-for="(item, i) in n.restart_config" :label="item.label" :prop="item.prop"
-                                       mode="inline">
-                            <lay-input :placeholder="item.placeholder"/>
-                        </lay-form-item>
-                    </lay-card>
-                </lay-form>
-            </lay-tab-item>
-        </lay-tab>
+<!--        <lay-tab type="card" v-model="currentTab">-->
+<!--            <lay-tab-item v-for="(n, index) in brokerConfig" :title="n.title" :id="index">-->
+<!--                <lay-form :model="brokerForm">-->
+<!--                    <lay-card v-if="n.dynamic_config&&n.dynamic_config.length>0" title="动态生效">-->
+<!--                        <lay-form-item v-for="(item, i) in n.dynamic_config" :label="item.label" :prop="item.prop"-->
+<!--                                       mode="inline">-->
+<!--                            <lay-input :placeholder="item.placeholder"/>-->
+<!--                        </lay-form-item>-->
+<!--                    </lay-card>-->
+<!--                    <lay-card v-if="n.restart_config&&n.restart_config.length>0" title="重启生效">-->
+<!--                        <lay-form-item v-for="(item, i) in n.restart_config" :label="item.label" :prop="item.prop"-->
+<!--                                       mode="inline">-->
+<!--                            <lay-input :placeholder="item.placeholder"/>-->
+<!--                        </lay-form-item>-->
+<!--                    </lay-card>-->
+<!--                </lay-form>-->
+<!--            </lay-tab-item>-->
+<!--        </lay-tab>-->
     </lay-layer>
 </template>
 
