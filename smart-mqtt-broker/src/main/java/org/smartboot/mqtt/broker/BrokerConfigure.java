f832fefe3657d468c7b95c2dc103dfe3a3618b14
三刀 <zhengjunweimail@163.com>
2023-04-30 10:24:34 +0800

    开发 0.20

--- smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/BrokerConfigure.java
+++ smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/BrokerConfigure.java
@@ -38,7 +38,7 @@ public class BrokerConfigure extends ToString {
     /**
      * 当前smart-mqtt
      */
-    public static final String VERSION = "v0.19";
+    public static final String VERSION = "v0.20";
 
     static final Map<String, String> SystemEnvironments = new HashMap<>();
 
