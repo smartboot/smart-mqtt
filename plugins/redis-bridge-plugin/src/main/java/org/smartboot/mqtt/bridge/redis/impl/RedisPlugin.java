2cf7ddcfc049fedddc5d08d9a85d47ac622a1128
三刀 <zhengjunweimail@163.com>
2023-05-05 13:15:50 +0800

    重新整理目录结构

--- plugins/redis-bridge-plugin/src/main/java/org/smartboot/mqtt/bridge/redis/impl/RedisPlugin.java
+++ plugins/redis-bridge-plugin/src/main/java/org/smartboot/mqtt/bridge/redis/impl/RedisPlugin.java
@@ -1,16 +1,26 @@
-package org.smartboot.mqtt.data.persistence.impl;
+/*
+ * Copyright (C) [2022] smartboot [zhengjunweimail@163.com]
+ *
+ *  企业用户未经smartboot组织特别许可，需遵循AGPL-3.0开源协议合理合法使用本项目。
+ *
+ *  Enterprise users are required to use this project reasonably
+ *  and legally in accordance with the AGPL-3.0 open source agreement
+ *  without special permission from the smartboot organization.
+ */
+
+package org.smartboot.mqtt.bridge.redis.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smartboot.mqtt.bridge.redis.DataPersistPlugin;
+import org.smartboot.mqtt.bridge.redis.config.DataSourcePluginConfig;
+import org.smartboot.mqtt.bridge.redis.handler.BrokerHandler;
+import org.smartboot.mqtt.bridge.redis.nodeinfo.MessageNodeInfo;
 import org.smartboot.mqtt.broker.BrokerContext;
 import org.smartboot.mqtt.broker.eventbus.ServerEventType;
 import org.smartboot.mqtt.broker.eventbus.messagebus.MessageBus;
 import org.smartboot.mqtt.broker.plugin.PluginException;
 import org.smartboot.mqtt.common.eventbus.EventBus;
-import org.smartboot.mqtt.data.persistence.DataPersistPlugin;
-import org.smartboot.mqtt.data.persistence.config.DataSourcePluginConfig;
-import org.smartboot.mqtt.data.persistence.handler.BrokerHandler;
-import org.smartboot.mqtt.data.persistence.nodeinfo.MessageNodeInfo;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
