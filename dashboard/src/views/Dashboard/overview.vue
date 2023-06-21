f553b575bad6a87091e8aaf1dd388260e7340783
三刀 <zhengjunweimail@163.com>
2023-06-21 18:52:57 +0800

    优化页面

--- dashboard/src/views/Dashboard/overview.vue
+++ dashboard/src/views/Dashboard/overview.vue
@@ -1,229 +1,225 @@
 <template>
-    <lay-row space="10">
-        <lay-col md="24" sm="24" xs="24">
-            <lay-field title="资源指标">
-                <lay-card>
-                    <lay-row space="10">
-                        <lay-col md="8">
-                            <a>
-                                <h3>连接数</h3>
-                                <p>
-                                    <cite>
-                                        <h1>
-                                            <lay-count-up :end-val="metric.client_online.value"
-                                                          :duration="2000"></lay-count-up>
-                                        </h1>
-                                    </cite>
-                                </p>
-                            </a>
-                        </lay-col>
-                        <lay-col md="8">
-                            <a>
-                                <h3>主题数</h3>
-                                <p>
-                                    <cite>
-                                        <h1>
-                                            <lay-count-up :end-val="metric.topic_count.value"
-                                                          :duration="2000"></lay-count-up>
-                                        </h1>
-                                    </cite>
-                                </p>
-                            </a>
-                        </lay-col>
-                        <lay-col md="8">
-                            <a>
-                                <h3>订阅数</h3>
-                                <p>
-                                    <cite>
-                                        <lay-count-up :end-val="metric.subscribe_topic_count.value"
-                                                      :duration="2000"></lay-count-up>
-                                    </cite>
-                                </p>
-                            </a>
-                        </lay-col>
-                    </lay-row>
-                </lay-card>
-            </lay-field>
-        </lay-col>
-        <lay-col md="12" sm="24" xs="24">
-            <lay-card>
-                <template #title><p>在线连接数：</p></template>
-                <Bar v-if="onlineClientChart" :data="onlineClientChart" :options="options"/>
-            </lay-card>
-        </lay-col>
-        <lay-col md="12" sm="24" xs="24">
-            <lay-card>
-                <template #title><p>在线主题数：</p></template>
-                <Bar v-if="topicCountChart" :data="topicCountChart" :options="options"/>
-            </lay-card>
-        </lay-col>
-        <lay-col md="12" sm="24" xs="24">
-            <lay-card>
-                <template v-if="metric.period_message_received" #title><p>消息流入速率：
-                    {{ metric.period_message_received.value }}
-                    条/{{
-                    metric.period_message_received.period > 1 ? metric.period_message_received.period : ""
-                    }}秒</p>
-                </template>
-                <template #body>
-                    <Bar v-if="period_message_received_chart" :data="period_message_received_chart" :options="options"/>
-                    <lay-result v-if="!period_message_received_chart" status="failure"
-                                :describe="message" title="消息流入速率"></lay-result>
-                </template>
-            </lay-card>
-        </lay-col>
-        <lay-col md="12" sm="24" xs="24">
-            <lay-card>
-                <template v-if="metric.period_message_sent" #title><p>消息流出速率：
-                    {{ metric.period_message_sent.value }}
-                    条/{{ metric.period_message_sent.period > 1 ? metric.period_message_sent.period : "" }}秒</p>
-                </template>
-                <template #body>
-                    <Bar v-if="period_message_sent_chart" :data="period_message_sent_chart" :options="options"/>
-                    <lay-result v-if="!period_message_sent_chart" status="failure" :describe="message"
-                                title="消息流出速率"></lay-result>
-                </template>
-            </lay-card>
-        </lay-col>
-
-
-    </lay-row>
+  <lay-row space="10">
+    <lay-col md="24" sm="24" xs="24">
+      <lay-field title="资源指标">
+        <lay-card>
+          <lay-row space="10">
+            <lay-col md="8">
+              <a>
+                <h3>连接数</h3>
+                <p>
+                  <cite>
+                    <h1>
+                      <lay-count-up :end-val="metric.client_online.value"
+                                    :duration="2000"></lay-count-up>
+                    </h1>
+                  </cite>
+                </p>
+              </a>
+            </lay-col>
+            <lay-col md="8">
+              <a>
+                <h3>主题数</h3>
+                <p>
+                  <cite>
+                    <h1>
+                      <lay-count-up :end-val="metric.topic_count.value"
+                                    :duration="2000"></lay-count-up>
+                    </h1>
+                  </cite>
+                </p>
+              </a>
+            </lay-col>
+            <lay-col md="8">
+              <a>
+                <h3>订阅数</h3>
+                <p>
+                  <cite>
+                    <lay-count-up :end-val="metric.subscribe_topic_count.value"
+                                  :duration="2000"></lay-count-up>
+                  </cite>
+                </p>
+              </a>
+            </lay-col>
+          </lay-row>
+        </lay-card>
+      </lay-field>
+    </lay-col>
+    <lay-col md="24" sm="24" xs="24">
+      <lay-card>
+        <template #title><p>集群拓扑：</p></template>
+        <div id="echarts-amap" ref="chinaRef"></div>
+      </lay-card>
+    </lay-col>
+    <lay-col md="12" sm="24" xs="24" v-for="(metric,key) in metrics" :key="key">
+      <lay-card>
+        <div class="chart" :ref="metric.chartRef"/>
+      </lay-card>
+    </lay-col>
+  </lay-row>
 
 </template>
 
 <script lang="ts">
-import {onMounted, ref} from "vue";
+import {ref} from "vue";
 import {dashboard_overview} from "../../api/module/api";
 import {onUnmounted} from "@vue/runtime-core";
-import {BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip} from 'chart.js'
-import {Bar} from 'vue-chartjs'
+import * as echarts from 'echarts';
+import {EChartsType} from 'echarts';
+import 'echarts-extension-amap';
 
 interface Metric {
-    code?: string
-    desc?: string
-    period?: number
-    time?: string
-    value: number
+  code?: string
+  desc?: string
+  period?: number
+  time?: string
+  value: number
 }
 
-interface MetricGroup {
-    client_online: Metric
-    topic_count: Metric
-    subscribe_topic_count: Metric
+interface MetricModel {
+  key: string,
+  chartRef: string,
+  chart?: EChartsType
+  queue?: Metric[]
 }
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
-
-export default {
-    components: {
-        Bar
+const updateChart = (model: MetricModel, metric: Metric) => {
+  const metricRef = model.chart
+  let historyQueue = model.queue || []
+  if (historyQueue.length > 0 && historyQueue[historyQueue.length - 1].time === metric.time) {
+    historyQueue[historyQueue.length - 1] = metric;
+  } else {
+    historyQueue.push(metric)
+  }
+  if (historyQueue.length >= 20) {
+    historyQueue.shift()
+  }
+  model.queue = historyQueue
+  metricRef?.hideLoading()
+  metricRef?.setOption({
+    title: {
+      text: metric.desc
     },
-    setup() {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {type: 'cross'}
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: historyQueue.map((item: Metric) => item?.time?.substring(11))
+    },
+    yAxis: {
+      // boundaryGap: [0, '50%'],
+      type: 'value',
+      axisLabel: {
+        formatter: '{value}'
+      }
+    },
+    series: [
+      {
+        name: '数量',
+        type: 'line',
+        smooth: true,
+        // symbol: 'none',
+        stack: 'a',
+        areaStyle: {
+          // normal: {}
+        },
+        data: historyQueue.map((item: Metric) => item.value)
+      }
+    ]
+  })
 
-        const message = "升级企业版解锁该指标视图";
-        const chartGroup = {}
+}
+export default {
+  mounted() {
 
-        const onlineClientChart = ref();
-        const topicCountChart = ref();
-        const period_message_received_chart = ref();
-        const period_message_sent_chart = ref();
-        const license=ref({
-            username:"",
-            password:"",
-            desc:"",
-            datetime:[]
-
-        });
-        const options = ref({
-            responsive: true,
-            maintainAspectRatio: false
-        })
-
-        const metric = ref<Record<string, Metric>>({
-            client_online: {value: 0},
-            topic_count: {value: 0},
-            subscribe_topic_count: {value: 0}
-        })
-
-        let timer;
-
-        onMounted(() => {
-            const loadData = async () => {
-                const {data} = await dashboard_overview();
-                console.log(data.metric);
-                metric.value = data.metric
-
-                //客户端在线连接数
-                updateChart(onlineClientChart, 'client_online', metric.value.client_online)
-
-                //在线主题连接数
-                updateChart(topicCountChart, 'topic_count', metric.value.topic_count)
-
-                //流入速率
-                if (metric.value.period_message_received) {
-                    updateChart(period_message_received_chart, 'period_message_received_queue', metric.value.period_message_received)
-                }
-
-                // //流出速率
-                if (metric.value.period_message_sent) {
-                    updateChart(period_message_sent_chart, 'period_message_sent', metric.value.period_message_sent)
-                }
-            };
-
-            /**
-             * 刷新Chart
-             */
-            const updateChart = (metricRef: any, metricKey: string, metric: Metric) => {
-                let historyQueue = chartGroup[metricKey] || []
-                if (historyQueue.length > 0 && historyQueue[historyQueue.length - 1].time === metric.time) {
-                    historyQueue[historyQueue.length - 1] = metric;
-                } else {
-                    historyQueue.push(metric)
-                }
-                if (historyQueue.length >= 20) {
-                    historyQueue.shift()
-                }
-                chartGroup[metricKey] = historyQueue
-                console.log(historyQueue);
-                metricRef.value = {
-                    labels: historyQueue.map((item: Metric) => item?.time?.substring(11)),
-                    datasets: [
-                        {
-                            label: historyQueue[0]["desc"],
-                            backgroundColor: '#f87979',
-                            data: historyQueue.map((item: Metric) => item.value)
-                        }
-                    ]
-                }
-            }
-
-            loadData()
-            timer = setInterval(() => {
-                loadData()
-            }, 5000)
-        });
-        onUnmounted(() => {
-            console.log("clear timer")
-            clearInterval(timer)
-        })
-        return {
-            onlineClientChart,
-            topicCountChart,
-            period_message_received_chart,
-            period_message_sent_chart,
-            message,
-            metric,
-            options,
-            license
+    const defaultOption = {
+      title: {
+        text: '数据加载中...'
+      },
+      tooltip: {},
+      legend: {
+        data: ['数量']
+      },
+      xAxis: {
+        data: []
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '数量',
+          type: 'bar',
+          data: []
         }
+      ]
+    };
+    this.metrics.map((metric) => {
+      metric.chart = echarts.init(this.$refs[metric.chartRef][0])
+      metric.chart.setOption(defaultOption)
+      metric.chart.showLoading();
+    })
+
+  },
+  setup() {
+    const items = ['client_online', 'topic_count', 'period_message_received', 'period_message_sent'];
+    const metrics: MetricModel[] = items.map(key => {
+      return {key: key, chartRef: 'chart_' + key}
+    })
+
+    const license = ref({
+      username: "",
+      password: "",
+      desc: "",
+      datetime: []
+
+    });
+
+    const metric = ref<Record<string, Metric>>({
+      client_online: {value: 0},
+      topic_count: {value: 0},
+      subscribe_topic_count: {value: 0}
+    })
+
+    const loadData = async () => {
+      const {data} = await dashboard_overview();
+      metric.value = data.metric
+
+      metrics.map(metric => {
+        updateChart(metric, data.metric[metric.key])
+      })
+
+    };
+
+    /**
+     * 刷新Chart
+     */
+    loadData()
+    let timer = setInterval(() => {
+      loadData()
+    }, 5000)
+    onUnmounted(() => {
+      console.log("clear timer")
+      clearInterval(timer)
+    })
+    return {
+      metrics,
+      metric,
+      license
     }
+  }
 }
 
 </script>
 <style>
 .flowChart {
-    width: 100%;
-    /*height: 150px;*/
+  width: 100%;
+  /*height: 150px;*/
 }
 
+.chart {
+  height: 300px;
+  width: 100%;
+}
 </style>
