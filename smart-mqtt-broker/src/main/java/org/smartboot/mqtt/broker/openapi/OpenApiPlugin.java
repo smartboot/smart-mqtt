14cd6dae872b55f0b347254fba0be25ca7692bf1
三刀 <zhengjunweimail@163.com>
2023-06-24 15:06:08 +0800

    优化代码

--- smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/openapi/OpenApiPlugin.java
+++ smart-mqtt-broker/src/main/java/org/smartboot/mqtt/broker/openapi/OpenApiPlugin.java
@@ -16,6 +16,7 @@ import org.smartboot.http.restful.RestfulBootstrap;
 import org.smartboot.http.restful.StaticResourceHandler;
 import org.smartboot.http.server.HttpBootstrap;
 import org.smartboot.mqtt.broker.BrokerContext;
+import org.smartboot.mqtt.broker.eventbus.ServerEventType;
 import org.smartboot.mqtt.broker.plugin.Plugin;
 import org.smartboot.mqtt.broker.plugin.PluginException;
 import org.smartboot.socket.enhance.EnhanceAsynchronousChannelProvider;
@@ -58,12 +59,14 @@ public class OpenApiPlugin extends Plugin {
             restfulBootstrap.addBean("brokerContext", brokerContext)
                     .scan("org.smartboot.mqtt.broker.openapi");
 
-            HttpBootstrap bootstrap = restfulBootstrap.bootstrap();
-            bootstrap.setPort(config.getPort());
-            bootstrap.configuration().bannerEnabled(false).host(config.getHost()).readBufferSize(1024 * 8).group(asynchronousChannelGroup);
-
-            bootstrap.start();
             brokerContext.getProviders().setOpenApiBootStrap(restfulBootstrap);
+
+            brokerContext.getEventBus().subscribe(ServerEventType.BROKER_STARTED, (eventType, object) -> {
+                HttpBootstrap bootstrap = brokerContext.getProviders().getOpenApiBootStrap().bootstrap();
+                bootstrap.setPort(config.getPort());
+                bootstrap.configuration().bannerEnabled(false).host(config.getHost()).readBufferSize(1024 * 8).group(asynchronousChannelGroup);
+                bootstrap.start();
+            });
             LOGGER.info("openapi server start success!");
         } catch (Exception e) {
             LOGGER.error("start openapi exception", e);
