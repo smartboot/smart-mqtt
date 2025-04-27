1f37dec63a7af4346097938266a933f485eb08cf
三刀 <zhengjunweimail@163.com>
2025-04-27 17:02:12 +0800

    开发插件管理功能

---
+++ smart-mqtt-plugin-enterprise/src/main/resources/static/assets/index.2c5584bb.js
@@ -0,0 +1,3 @@
+import {C as n} from "./index.4916e789.js";
+
+n.install=function(o){o.component(n.name,n)};
--- smart-mqtt-plugin-enterprise/src/main/resources/static/assets/index.c3346299.js
+++
@@ -1,3 +0,0 @@
-import {C as n} from "./index.e38f6ab9.js";
-
-n.install=function(o){o.component(n.name,n)};
